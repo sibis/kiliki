087bfffb59a97e64c060bc6983c91476dfa0a653
Sibi <sibi.kabil@gmail.com>
2020-11-22 14:58:13 +0530

    NextToken method impl with tests updated for token

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,15 +1,17 @@
-package lexer 
+package lexer
 
 import (
 	"testing"
+
 	"github.com/sibis/kiliki/token"
 )
 
 func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
 	tests := []struct {
-		expectedType token.TokenType expectedLiteral string
-		}{
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
 		{token.ASSIGN, "="},
 		{token.PLUS, "+"},
 		{token.LPAREN, "("},
@@ -21,13 +23,15 @@ func TestNextToken(t *testing.T) {
 		{token.EOF, ""},
 	}
 	l := New(input)
-	for i, tt := range tests { tok := l.NextToken()
+	for i, tt := range tests {
+		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-		t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-					i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
 		}
 		if tok.Literal != tt.expectedLiteral {
-		t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-		} 
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
 	}
 }
